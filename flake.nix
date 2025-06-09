beaf122e86847443a10ab4120037311969e0598d
Aditya <bluenerd@protonmail.com>
2025-06-09 12:20:53 +0530

    h

--- flake.nix
+++ flake.nix
@@ -11,30 +11,25 @@
   outputs = { self, nixpkgs, flake-utils, rust-overlay, nixvim, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-      	config = import ./config;
+        config = import ./config;
         overlays = [ rust-overlay.overlays.default ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = import nixpkgs { inherit system overlays; };
 
         rustToolchain = pkgs.rust-bin.stable.latest.default;
 
-	nixvimlib = nixvim.lib.${system};
+        nixvimlib = nixvim.lib.${system};
         vv = nixvim.legacyPackages.x86_64-linux.makeNixvimWithModule {
-	inherit pkgs;
-	module = config;
+          inherit pkgs;
+          module = config;
         };
-      in
-      {
+      in {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "shell-rs";
           version = "0.1.0";
 
           src = ./.;
 
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
+          cargoLock = { lockFile = ./Cargo.lock; };
         };
         devShells.default = pkgs.mkShell {
           buildInputs = [
@@ -52,7 +47,6 @@
             echo "Welcome to your Rust development environment!"
           '';
         };
-      }
-    );
+      });
 }
 
